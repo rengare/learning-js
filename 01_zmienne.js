d3b78af78626626df0f52106c612d77f4d3551c8
Gracjan Gorecki <gorecki.gracjan@hotmail.com>
2021-08-06 15:48:42 +0200

    feat(lesson): 01 variables

---
+++ 01_zmienne.js
@@ -0,0 +1,11 @@
+let a = 1 // integer 
+let b = 1.1 // float
+let toJestJakisTekst = ' witaj swiecie ' // string
+let toJestJakisTekst2 = " witaj swiecie " // string
+
+console.log(a)
+console.log(b)
+
+console.log(toJestJakisTekst)
+console.log(toJestJakisTekst2)
+
